b5a5f5ae8fe19af67244c5278314f080a2eca6d9
nexpang <rlarudgur123@gmail.com>
2021-04-12 00:58:37 +0900

    소켓 추가할려다가 포기

--- Node js_study/dev/app.js
+++ Node js_study/dev/app.js
@@ -1,25 +1,32 @@
 const express = require("express");
 const hbs = require("express-handlebars");
 
-const server = express();
+const app = express();
 
-server.engine("hbs", hbs({
+app.engine("hbs", hbs({
     extname: "hbs",
     defaultLayout: "layout.hbs",
     partialsDir: "partials",
-})
-);
+}));
 
-server.set("view engine", "hbs");
-server.use(express.static(__dirname + "/public"));
+app.set("view engine", "hbs");
+app.use(express.static(__dirname + "/public"));
 
-server.get("/", (req, res) => {
+app.get("/", (req, res) => {
     res.render("home", {
         msg: "Hello from node.js"
     });
 })
+app.get("/chat1", (req, res) => {
+    res.render("chat", {
+        msg: "Hello from node.js"
+    });
+})
+app.use((req, res) => {
+    res.render("404");
+})
 
-server.listen(3000, (err) => {
+app.listen(3000, (err) => {
     if (err)
         return console.log(err);
     console.log("server is listening from port 3000");
